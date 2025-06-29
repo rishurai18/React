1b889f0f48b5ede8580484dac82c359eefd559b6
rishurai18 <rairishuk@gmail.com>
2025-06-29 15:43:22 +0000

    ok

--- demoproject/src/App.js
+++ demoproject/src/App.js
@@ -3,7 +3,28 @@ import './App.css';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+
+      <h1>Hello World in React</h1>
+     <table border={1}>
+            <thead>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Salary</th>
+            </thead>
+            <tr>
+                <td>Sumant Rai</td>
+                <td>20</td>
+                <td>90000</td>
+            </tr>
+            <tr>
+                <td>Apeksha More</td>
+                <td>20</td>
+                <td>50000</td>
+            </tr>
+        </table>
+        
+        </div>
   );
 }
 
--- demoproject/src/index.js
+++ demoproject/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import first from './first';
+// import first from './first';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
