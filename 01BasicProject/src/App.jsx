4f511796e02524b204845096bebd95c31db5bcff
rishurai18 <rairishuk@gmail.com>
2025-06-29 16:04:58 +0000

    basic project 1

---
+++ 01BasicProject/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+
+
+
+
+function App() {
+  
+
+  return (
+    <>
+     <h1>I Love You Apeksha</h1>
+     <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis nihil odit non facere ratione aspernatur molestiae excepturi cum nobis magni? Odio ut tempora nihil quaerat incidunt. Quam quidem accusantium soluta?</h2>
+    
+    </>
+  )
+}
+
+export default App
